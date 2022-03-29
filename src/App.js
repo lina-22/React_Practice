c45a70736b4d86daed4a81884a9ffe827a8435a3
lina-22 <haquefahinur@gmail.com>
2022-03-30 01:13:28 +0200

    Bismillahir rahmanir rahim

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <h3>Bismillahir rahmanir rahim React Practicig</h3>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React Lina
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
