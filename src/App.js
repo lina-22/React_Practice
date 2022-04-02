a39ecfae23caa8d4aed519725034a2d68dcffddb
lina-22 <haquefahinur@gmail.com>
2022-04-02 10:41:19 +0200

    components and function create

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './mycomponents/Hello.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+// function App() {
+//   // return (
+//   //   // <div className="App">
+//   //   //   {/* <header className="App-header">
+//   //   //     <img src={logo} className="App-logo" alt="logo" />
+//   //   //     <p>
+//   //   //       Edit <code>src/App.js</code> and save to reload.
+//   //   //     </p>
 
-        <h3>Bismillahir rahmanir rahim React Practicig</h3>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Lina
-        </a>
-      </header>
-    </div>
-  );
+//   //   //     <h3>Bismillahir rahmanir rahim React Practicig start</h3>
+//   //   //     <a
+//   //   //       className="App-link"
+//   //   //       href="https://reactjs.org"
+//   //   //       target="_blank"
+//   //   //       rel="noopener noreferrer"
+//   //   //     >
+//   //   //       Learn React Lina
+//   //   //     </a>
+//   //   //   </header> */}
+//   //   // </div>
+//   // );
+
+
+// }
+
+
+function App(){
+   return (
+     <div className="App">
+         <Hello></Hello>
+     </div>
+   );
 }
 
 export default App;
---
+++ src/mycomponents/Hello.js
@@ -0,0 +1,12 @@
+import { hasUnreliableEmptyValue } from '@testing-library/user-event/dist/utils';
+import React from 'react';
+
+function Hello(){
+    return (
+  
+         < h1>Hello I am from functional component</h1>
+
+    );
+}
+
+export default Hello;
