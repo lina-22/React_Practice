59d64e7ffb2702708b63a31f3adb596e7f98aabb
lina-22 <haquefahinur@gmail.com>
2022-04-09 02:50:26 +0200

    react practice

--- src/mycomponents/Hello.js
+++ src/mycomponents/Hello.js
@@ -2,9 +2,14 @@ import { hasUnreliableEmptyValue } from '@testing-library/user-event/dist/utils'
 import React from 'react';
 
 function Hello(props){
+    function DoThis(){
+        alert("Button is Clicked")
+    }
     return (
-  
+        <div>
+            <button onClick={DoThis}>click me</button>
          < h1>Name: {props.name} And Age:{props.age}</h1>
+        </div>
 
     );
 }
--- src/mycomponents/Welcome.js
+++ src/mycomponents/Welcome.js
@@ -3,6 +3,7 @@ import React,{Component} from "react";
 class Welcome extends Component{
 
     render(){
+        button 
         return <h1>I am from class component</h1>
     }
 }
